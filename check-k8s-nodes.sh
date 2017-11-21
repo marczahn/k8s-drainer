27ac532c5d234e1f64b4af5bafb888c157315619
Marc Zahn <info@marczahn.de>
2017-11-21 15:09:02 +0100

    Create check-k8s-nodes.sh

---
+++ check-k8s-nodes.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Get only nodes which are not drained yet
+NOT_READY_NODES=$(kubectl get nodes | grep -P 'NotReady(?!,SchedulingDisabled)' | awk '{print $1}' | xargs echo)
+# Get only nodes which are still drained
+READY_NODES=$(kubectl get nodes | grep '\sReady,SchedulingDisabled' | awk '{print $1}' | xargs echo)
+
+echo "Unready nodes that are undrained: $NOT_READY_NODES"
+echo "Ready nodes: $READY_NODES"
+
+
+for node in $NOT_READY_NODES; do
+  echo "Node $node not drained yet, draining..."
+  kubectl drain --ignore-daemonsets --force $node
+  echo "Done"
+done;
+
+for node in $READY_NODES; do
+  echo "Node $node still drained, uncordoning..."
+  kubectl uncordon $node
+  echo "Done"
+done;
